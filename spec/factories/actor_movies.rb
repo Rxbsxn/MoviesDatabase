4770cec0f180cc687c34a8d2ddcda8cba0af835e
Robert Harężlak <harezlak.robert7@gmail.com>
2017-06-21 14:55:48 +0200

    some changes in helper

--- spec/factories/actor_movies.rb
+++ spec/factories/actor_movies.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :actor_movie do
-    actor_id { Actor.all.sample.id }
-    movie_id { Movie.all.sample.id }
+    actor_id { Actor.first || create(:actor).id }
+    movie_id { Movie.first || create(:movie).id }
   end
 end
