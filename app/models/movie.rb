eccb1eb567decb1baa9a75d3861579dfd9e3511d
Robert Harężlak <harezlak.robert7@gmail.com>
2017-06-21 13:58:03 +0200

    relationship configuration

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,5 @@
 class Movie < ApplicationRecord
+  belongs_to :genre
+  has_many :actors, through: :actor_movies
+  has_many :actor_movies
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170621114304) do
+ActiveRecord::Schema.define(version: 20170621114415) do
 
   create_table "actor_movies", force: :cascade do |t|
     t.integer  "actor_id"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20170621114304) do
     t.integer  "revenue"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.         "genre_id"
   end
 
 end
