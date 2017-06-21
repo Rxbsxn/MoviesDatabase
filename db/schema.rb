df4d31afebac834c47969b5d548d4f9b5f3df039
Robert Harężlak <harezlak.robert7@gmail.com>
2017-06-21 13:43:47 +0200

    third table to connect actor & movie

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170621114018) do
+ActiveRecord::Schema.define(version: 20170621114304) do
+
+  create_table "actor_movies", force: :cascade do |t|
+    t.integer  "actor_id"
+    t.integer  "movie_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "actors", force: :cascade do |t|
     t.string   "first_name"
