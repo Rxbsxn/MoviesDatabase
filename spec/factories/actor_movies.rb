91be2b07170d18d2f16994bd28a6d1e12a8ef736
Robert Harężlak <harezlak.robert7@gmail.com>
2017-06-21 14:43:53 +0200

    factories

--- spec/factories/actor_movies.rb
+++ spec/factories/actor_movies.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :actor_movie do
-    
+    actor_id { Actor.all.sample.id }
+    movie_id { Movie.all.sample.id }
   end
 end
--- spec/factories/genres.rb
+++ spec/factories/genres.rb
@@ -1,5 +1,5 @@
 FactoryGirl.define do
   factory :genre do
-    
+    name { Faker::Hipster.word }
   end
 end
