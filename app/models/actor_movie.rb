0492cd63c67e6e4b197ac0cb0de0e5443d4ec21c
Robert Harężlak <harezlak.robert7@gmail.com>
2017-06-21 15:03:37 +0200

    validation & test for ActorMovie

--- app/models/actor_movie.rb
+++ app/models/actor_movie.rb
@@ -1,4 +1,7 @@
 class ActorMovie < ApplicationRecord
   belongs_to :actor
   belongs_to :movie
+  
+  validates :actor_id, presence: true
+  validates :movie_id, presence: true
 end
---
+++ spec/models/actor_movies_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe ActorMovie, type: :model do
+  describe 'ActorMovie count' do
+    it 'empty table' do
+      expect(ActorMovie.count).to eq 0
+    end
+  end
+  it 'one genre in table' do
+    create(:actor_movie)
+    expect(ActorMovie.count).to eq 1
+  end
+
+  describe 'ActorMovie is valid' do
+    let(:actor_movies) { create(:actor_movie) }
+    it 'returns valid record' do
+      expect(actor_movies).to be_valid
+    end
+  end
+
+  describe 'ActorMovie attribs validation' do
+    it { should validate_presence_of(:actor_id) }
+    it { should validate_presence_of(:movie_id) }
+  end
+
+  describe 'should have many movies and actor_movies' do
+    it { should belong_to(:movie) }
+    it { should belong_to(:actor) }
+  end  
+end
