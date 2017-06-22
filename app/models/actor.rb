4be9efcd276f1405dc4649acdc1dddf38ca9d582
Robert Harężlak <harezlak.robert7@gmail.com>
2017-06-22 14:33:19 +0200

    query scope

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -3,4 +3,10 @@ class Actor < ApplicationRecord
   has_many :movies, through: :actor_movies
 
   validates :first_name, :last_name, presence: true
+
+  scope :max_movies, -> { joins(:movies).group(:id).order("count(actor_id) desc").first.full_name }
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
 end
