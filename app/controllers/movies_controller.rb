643922c21755a531a9af90b5094f68d86b8d2e99
Robert Harężlak <harezlak.robert7@gmail.com>
2017-06-22 12:38:09 +0200

    Movie CRUD

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,50 @@
+class MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.create(movie_params)
+    if @movie.save
+      redirect_to movies_path
+    else
+      render :new
+    end
+  end
+  
+  def edit
+    movie
+  end
+
+  def update
+    movie
+    if movie.update(movie_params)
+      redirect_to movies_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    movie
+    if movie.destroy
+      redirect_to movies_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:name, :revenue, :genre_id)
+  end
+
+  def movie
+    @movie = Movie.find(params[:id])
+  end
+end
