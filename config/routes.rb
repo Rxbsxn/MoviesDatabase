67319600a253f3d6d4a0f736d2f7962480f281d8
Robert Harężlak <harezlak.robert7@gmail.com>
2017-06-22 14:33:02 +0200

    controller for statistics & routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+
   root 'index#index'
 
   resources :actors
   resources :movies
   resources :genres
+  resources :stats, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
