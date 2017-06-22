a7fde2f6b9018c863bd63188b1f845175ccc28b9
Robert Harężlak <harezlak.robert7@gmail.com>
2017-06-22 14:06:01 +0200

    apply feedback

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -3,4 +3,8 @@ class Actor < ApplicationRecord
   has_many :movies, through: :actor_movies
 
   validates :first_name, :last_name, presence: true
+
+  def three_movies
+    movies.take(3).pluck(:name).join(', ')  
+  end
 end
