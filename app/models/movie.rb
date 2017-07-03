beae3d999e9aeb5ca65c1f96bb80807af5a08630
Robert Harężlak <harezlak.robert7@gmail.com>
2017-07-03 11:57:58 +0200

    Movie model fix

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,10 +4,10 @@ class Movie < ApplicationRecord
   has_many :actor_movies
   has_many :awards
 
-  validates :name, :genre_id, :revenue, presence: true
+  validates :name, :genre, :revenue, presence: true
   validates :revenue, numericality: true
 
   scope :max_revenue, -> { order(revenue: :desc).first.name }
   scope :greatest_movie, -> { joins(:awards).group(:id)
-                             .order("count(movie_id) desc").first.name}
+                             .order("count(movie_id) desc").first.name }
 end
