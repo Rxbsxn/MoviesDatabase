0569fd8fa2649ecebc462ae25e8340e1cd4b2e60
Robert Harężlak <harezlak.robert7@gmail.com>
2017-07-30 11:44:33 +0200

    Allow to destroy records

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module MoviesDatabase
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :delete, :options]
       end
     end
     # Settings in config/environments/* take precedence over those specified here.
